ecaf887d8748a1f16d8d5c2759e33c9bd30d1bc0
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-01-16 21:51:22 +0200

    Upload logo

--- my-app/src/components/NavH.js
+++ my-app/src/components/NavH.js
@@ -3,8 +3,8 @@ const NavH = () => {
 	return (
 		<div className="Nav-H col-12 me-0 pe-0">
 			<nav className="navbar bg-body-tertiary">
-				<div className="container-fluid">
-					<span className="navbar-brand mb-0 h1">Navbar</span>
+				<div className="container-fluid row">
+					<span className="col-1 text-center">Paste & GO</span>
 				</div>
 			</nav>
 		</div>
