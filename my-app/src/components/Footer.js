fb8995404edb5a471e8c801f8e950e511c87c867
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-02-04 22:28:25 +0200

    Init Footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -2,7 +2,47 @@ import "./Footer.css";
 const Footer = () => {
 	return (
 		<div className="Footer">
-			<h1 className="text-center">this is footer</h1>
+			<ul className="social">
+				<li>
+					<a
+						href="https://www.linkedin.com/in/marwan-mohamed-636b25226/"
+						target={"_blank"}
+						rel="noreferrer"
+					>
+						<i class="fa-brands fa-linkedin-in"></i>
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://github.com/MaroMohamedSalah"
+						target={"_blank"}
+						rel="noreferrer"
+					>
+						<i class="fa-brands fa-github"></i>
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://www.instagram.com/maro_salah1/"
+						target={"_blank"}
+						rel="noreferrer"
+					>
+						<i class="fa-brands fa-instagram"></i>
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://www.facebook.com/profile.php?id=100009187233222"
+						target={"_blank"}
+						rel="noreferrer"
+					>
+						<i class="fa-brands fa-facebook-f"></i>
+					</a>
+				</li>
+			</ul>
+			<h2 className="text-center">
+				ENG.Marwan Mohamed @<span>2023</span>
+			</h2>
 		</div>
 	);
 };
