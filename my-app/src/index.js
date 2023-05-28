0faf97ff800a5a7d066f69593cb48bde9b3d414e
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-05-28 13:26:21 +0300

    fix bugs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,22 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { RecoilRoot } from "recoil";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const history = createBrowserHistory();
+
+ReactDOM.render(
 	<React.StrictMode>
 		<RecoilRoot>
-			<App />
+			<BrowserRouter history={history}>
+				<App />
+			</BrowserRouter>
 		</RecoilRoot>
-	</React.StrictMode>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
