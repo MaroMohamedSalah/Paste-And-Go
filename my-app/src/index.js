6de27aa5468b3eabdccc98b7221b47cc3f9b0f9d
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-05-28 13:28:29 +0300

    fix bugs

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,24 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { RecoilRoot } from "recoil";
 
-const history = createBrowserHistory();
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
-ReactDOM.render(
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyBzlLFSDCNUN9uVE8KhyU6VqHcw3AZbCxU",
+// 	authDomain: "paste-and-go.firebaseapp.com",
+// 	projectId: "paste-and-go",
+// 	storageBucket: "paste-and-go.appspot.com",
+// 	messagingSenderId: "249497879782",
+// 	appId: "1:249497879782:web:e077cad0006ccdcf446727",
+// 	measurementId: "G-G3B1KHQ8SB",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		<RecoilRoot>
-			<BrowserRouter history={history}>
-				<App />
-			</BrowserRouter>
+			<App />
 		</RecoilRoot>
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
 
 reportWebVitals();
