15e7412b9f50a598627dd90ca2e43f3b559a3622
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-06-25 17:16:59 +0300

    fix bugs

--- my-app/src/layouts/Home-layout.js
+++ my-app/src/layouts/Home-layout.js
@@ -1,5 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavH from "../components/NavH";
+import NavV from "../components/NavV";
+import Footer from "../components/Footer";
+
 const HomeLayout = () => {
-	return;
+	return (
+		<>
+			<NavH />
+			<NavV />
+			<Outlet />
+			<Footer />
+		</>
+	);
 };
 
 export default HomeLayout;
