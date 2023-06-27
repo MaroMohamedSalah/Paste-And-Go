6fc883aac04d9fc0720b8f83f19d2db058f1b59b
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-06-27 13:59:16 +0300

    refine layouts

--- my-app/src/layouts/Facebook-layout.js
+++ my-app/src/layouts/Facebook-layout.js
@@ -1,25 +1,33 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import Steps from "../Atoms/Steps";
 import FacebookUrl from "../components/Facebook-url";
+
 const FacebookLayout = () => {
 	const [step, setStep] = useRecoilState(Steps);
+
+	const handleStep = () => {
+		setStep(1);
+	};
+
 	return (
 		<div className="Facebook urlGetter">
 			<form className="facebook-url media row w-lg-50 w-md-50 w-sm-90">
 				<FacebookUrl />
 				<div className="MP3 col-lg-6 col-12 p-sm-5">
-					<Link to="mp3" onClick={() => setStep(1)}>
-						<button type="submit">MP3</button>
+					<Link to="mp3" onClick={handleStep}>
+						<button type="button">MP3</button>
 					</Link>
 				</div>
 				<div className="MP3 col-lg-6 col-12 p-sm-5">
-					<Link to="mp4" onClick={() => setStep(1)}>
-						<button type="submit">MP4</button>
+					<Link to="mp4" onClick={handleStep}>
+						<button type="button">MP4</button>
 					</Link>
 				</div>
 			</form>
 		</div>
 	);
 };
+
 export default FacebookLayout;
--- my-app/src/layouts/Youtube-layout.js
+++ my-app/src/layouts/Youtube-layout.js
@@ -2,19 +2,21 @@ import { Link } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import Steps from "../Atoms/Steps";
 import YouTubeUrl from "../components/YouTube-url";
+
 const YouTubeLayout = () => {
 	const [step, setStep] = useRecoilState(Steps);
+
 	return (
 		<div className="Youtube urlGetter">
 			<form className="youtube-url media row w-lg-50 w-md-50 w-sm-90">
 				<YouTubeUrl />
 				<div className="MP3 col-lg-6 col-12 p-sm-5">
-					<Link to={"mp3"} onClick={() => setStep(1)}>
+					<Link to="mp3" onClick={() => setStep(1)}>
 						<button type="submit">MP3</button>
 					</Link>
 				</div>
 				<div className="MP3 col-lg-6 col-12 p-sm-5">
-					<Link to={"mp4"} onClick={() => setStep(1)}>
+					<Link to="mp4" onClick={() => setStep(1)}>
 						<button type="submit">MP4</button>
 					</Link>
 				</div>
@@ -22,4 +24,5 @@ const YouTubeLayout = () => {
 		</div>
 	);
 };
+
 export default YouTubeLayout;
